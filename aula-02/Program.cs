3a87f1f6457fdd62accfc22eaf23f344169c5714
MrGalva07 <lucas.galvao0707@gmail.com>
2026-03-18 11:10:01 -0300

    first commit

---
+++ aula-02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    
+    static void Main(string[]args){
+        Console.Write("Oi,maxu");
+        if(args.GetLength(0)>0 )  {
+            Console.Write(args.GetValue(0));
+        }
+        else
+        {
+            Console.Write("Falta um parametro");
+        }
+        
+    }
+};
---
+++ aula-03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Aula03
+{
+    static void Main()
+    {
+
+        int num1,num2,res;
+        num1 = 10;
+        num2 = 20;
+
+        res = num1 + num2;
+
+        char letra= 'a';
+        bool boleano = true;
+        float flutuante = 3.2f;
+        string name = "maxueru";
+
+        var aux = name;
+
+             Console.WriteLine("nome :"+ aux);
+             Console.WriteLine("a soma de " + num1 + " + " + num2 +" É igual a: " +res );
+             
+        
+    }
+    
+}
