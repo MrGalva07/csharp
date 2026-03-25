f077fb816b4757985c5214cd31004f9abff9aa5e
MrGalva07 <lucas.galvao0707@gmail.com>
2026-03-25 17:54:53 -0300

    switch and consts

---
+++ aula-07/switchcase/Program.cs
@@ -0,0 +1,34 @@
+﻿///SWITCH CASE 
+
+Console.WriteLine("digite um número de 1 a 7 para saber qual o dia correspondente : ");
+string input = Console.ReadLine();
+
+switch (input)
+{
+    case "1":
+    Console.WriteLine("Domingo");
+    break;
+    case "2":
+    Console.WriteLine("Segunda");
+    break;
+
+case "3":
+    Console.WriteLine("Terça");
+    break;
+    case "4":
+    Console.WriteLine("Quarta");
+    break;
+    case "5":
+    Console.WriteLine("Quinta");
+    break;
+    case "6":
+    Console.WriteLine("Sexta");
+    break;
+    case "2":
+    Console.WriteLine("Sabado");
+    break;
+    default:
+    Console.WriteLine("Digite um número valido de 1 a 7");
+    break;
+
+}
---
+++ aula-08/const/Program.cs
@@ -0,0 +1,7 @@
+﻿const int PRECO_1=10;
+const int PRECO_2 =20;
+
+const double tax = 0.20;
+
+double totalCost = (PRECO_1 + PRECO_2)*tax;
+Console.WriteLine(totalCost);
